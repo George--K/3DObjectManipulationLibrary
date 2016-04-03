63de483cda3d17c6191ce381b39887025f873e1a
George-K <juranya@gmail.com>
2016-04-03 21:00:58 +0300

    added shaders, set up transformation matrices and states, reworked the engine for textured objects;

--- TestingApp/TestingApp/main.cpp
+++ TestingApp/TestingApp/main.cpp
@@ -11,109 +11,126 @@
 #define SCREEN_WIDTH  1366
 #define SCREEN_HEIGHT 768
 
-IDXGISwapChain *swapchain;
-ID3D11Device *dev;
-ID3D11DeviceContext *devcon;
-ID3D11RenderTargetView *backbuffer;
+IDXGISwapChain *swapÑhain;
+ID3D11Device *d3dDeviceInterface;
+ID3D11DeviceContext *d3dDeviceContext;
+ID3D11RenderTargetView *backBuffer;
+ID3D11DepthStencilView *depthBuffer;
+ID3D11InputLayout *inputLayout;
+ID3D11VertexShader *vertexShader;
+ID3D11PixelShader *pixelShader;
+ID3D11Buffer *vertexBuffer;
+ID3D11Buffer *indexBuffer;
+ID3D11Buffer *constBuffer;
+ID3D11ShaderResourceView *texture;
+
+ID3D11RasterizerState *rasterizerState; 
+ID3D11SamplerState *samplerState;
+ID3D11BlendState *blendState;
+
+struct VERTEX { FLOAT X, Y, Z; D3DXVECTOR3 Normal; FLOAT U, V; };
+
+struct CONSTANT_BUFFER
+{
+	D3DXMATRIX finalTransformationMatrix;
+	D3DXMATRIX rotationMatrix;
+	D3DXVECTOR4 directionalLightVector;
+	D3DXCOLOR directionalLightColor;
+	D3DXCOLOR ambientLightColor;
+};
 
 void InitD3D(HWND hWnd);
 void RenderFrame(void);
 void CleanD3D(void);
+void InitGraphics(void);
+void InitPipeline(void);
+void InitStates(void);
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-
 
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	HWND hWnd;
-	WNDCLASSEX wc;
+	HWND windowHandle;
+	WNDCLASSEX windowClass;
 
-	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	ZeroMemory(&windowClass, sizeof(WNDCLASSEX));
 
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.lpszClassName = L"WindowClass";
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.lpszClassName = L"WindowClass";
 
-	RegisterClassEx(&wc);
+	RegisterClassEx(&windowClass);
 
-	RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
-	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+	RECT windowRectangle = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	AdjustWindowRect(&windowRectangle, WS_OVERLAPPEDWINDOW, FALSE);
 
-	hWnd = CreateWindowEx(NULL,
+	windowHandle = CreateWindowExW(NULL,
 		L"WindowClass",
-		L"Our First Direct3D Program",
+		L"Direct3D Testing App",
 		WS_OVERLAPPEDWINDOW,
-		300,
-		300,
-		wr.right - wr.left,
-		wr.bottom - wr.top,
+		0,
+		0,
+		windowRectangle.right - windowRectangle.left,
+		windowRectangle.bottom - windowRectangle.top,
 		NULL,
 		NULL,
 		hInstance,
 		NULL);
 
-	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(windowHandle, nCmdShow);
+	InitD3D(windowHandle);
 
-	InitD3D(hWnd);
-
-	MSG msg;
-
+	MSG message;
 	while (TRUE)
 	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		if (PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-
-			if (msg.message == WM_QUIT)
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+			if (message.message == WM_QUIT)
 				break;
 		}
-
 		RenderFrame();
 	}
 
 	CleanD3D();
-
-	return msg.wParam;
+	return message.wParam;
 }
 
-
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
-	case WM_DESTROY:
-	{
-		PostQuitMessage(0);
-		return 0;
-	} break;
+		case WM_DESTROY:
+				{
+					PostQuitMessage(0);
+					return 0;
+				}
+				break;
 	}
-
-	return DefWindowProc(hWnd, message, wParam, lParam);
+	return DefWindowProc(windowHandle, message, wParam, lParam);
 }
 
-
-void InitD3D(HWND hWnd)
+void InitD3D(HWND windowHandle)
 {
-	DXGI_SWAP_CHAIN_DESC scd;
+	DXGI_SWAP_CHAIN_DESC swapChainDescription;
+	ZeroMemory(&swapChainDescription, sizeof(DXGI_SWAP_CHAIN_DESC));
 
-	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
-
-	scd.BufferCount = 1;
-	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	scd.BufferDesc.Width = SCREEN_WIDTH; 
-	scd.BufferDesc.Height = SCREEN_HEIGHT;
-	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	scd.OutputWindow = hWnd;
-	scd.SampleDesc.Count = 4;
-	scd.Windowed = TRUE;
-	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	swapChainDescription.BufferCount = 1;
+	swapChainDescription.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDescription.BufferDesc.Width = SCREEN_WIDTH;
+	swapChainDescription.BufferDesc.Height = SCREEN_HEIGHT;
+	swapChainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDescription.OutputWindow = windowHandle;
+	swapChainDescription.SampleDesc.Count = 4;
+	swapChainDescription.Windowed = FALSE;
+	swapChainDescription.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
 	D3D11CreateDeviceAndSwapChain(NULL,
 		D3D_DRIVER_TYPE_HARDWARE,
@@ -122,42 +139,273 @@ void InitD3D(HWND hWnd)
 		NULL,
 		NULL,
 		D3D11_SDK_VERSION,
-		&scd,
-		&swapchain,
-		&dev,
+		&swapChainDescription,
+		&swapÑhain,
+		&d3dDeviceInterface,
 		NULL,
-		&devcon);
+		&d3dDeviceContext);
 
-	ID3D11Texture2D *pBackBuffer;
-	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	D3D11_TEXTURE2D_DESC textureDescription;
+	ZeroMemory(&textureDescription, sizeof(textureDescription));
 
-	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
-	pBackBuffer->Release();
+	textureDescription.Width = SCREEN_WIDTH;
+	textureDescription.Height = SCREEN_HEIGHT;
+	textureDescription.ArraySize = 1;
+	textureDescription.MipLevels = 1;
+	textureDescription.SampleDesc.Count = 4;
+	textureDescription.Format = DXGI_FORMAT_D32_FLOAT;
+	textureDescription.BindFlags = D3D11_BIND_DEPTH_STENCIL;
 
-	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+	ID3D11Texture2D *textureDepthBuffer;
+	d3dDeviceInterface->CreateTexture2D(&textureDescription, NULL, &textureDepthBuffer);
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDescription;
+	ZeroMemory(&depthStencilViewDescription, sizeof(depthStencilViewDescription));
+	depthStencilViewDescription.Format = DXGI_FORMAT_D32_FLOAT;
+	depthStencilViewDescription.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+
+	d3dDeviceInterface->CreateDepthStencilView(textureDepthBuffer, &depthStencilViewDescription, &depthBuffer);
+	d3dDeviceInterface->Release();
+	ID3D11Texture2D *textureBackBuffer;
+	swapÑhain->GetBuffer(NULL, __uuidof(ID3D11Texture2D), (LPVOID*)&textureBackBuffer);
+
+	d3dDeviceInterface->CreateRenderTargetView(textureBackBuffer, NULL, &backBuffer);
+	d3dDeviceContext->OMSetRenderTargets(1, &backBuffer, depthBuffer);
 
 	D3D11_VIEWPORT viewport;
 	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
-
 	viewport.TopLeftX = 0;
 	viewport.TopLeftY = 0;
 	viewport.Width = SCREEN_WIDTH;
 	viewport.Height = SCREEN_HEIGHT;
+	viewport.MinDepth = 0;
+	viewport.MaxDepth = 1;
+	d3dDeviceContext->RSSetViewports(1, &viewport);
 
-	devcon->RSSetViewports(1, &viewport);
+	InitPipeline();
+	InitGraphics();
+	InitStates();
 }
 
 void RenderFrame(void)
 {
-	devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f));
-	swapchain->Present(0, 0);
+	CONSTANT_BUFFER constantBuffer;
+
+	constantBuffer.directionalLightVector = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 0.0f);
+	constantBuffer.directionalLightColor = D3DXCOLOR(0.5f, 0.5f, 0.5f, 1.0f);
+	constantBuffer.ambientLightColor = D3DXCOLOR(0.2f, 0.2f, 0.2f, 1.0f);
+
+	D3DXMATRIX matRotate, matView, matProjection;
+	D3DXMATRIX matFinal;
+
+	static float Time = 0.0f; Time += 0.0003f;
+	D3DXMatrixRotationY(&matRotate, Time);
+
+	D3DXMatrixLookAtLH(&matView,
+		&D3DXVECTOR3(0.0f, 3.0f, 5.0f),
+		&D3DXVECTOR3(0.0f, 0.0f, 0.0f),
+		&D3DXVECTOR3(0.0f, 1.0f, 0.0f));
+
+	D3DXMatrixPerspectiveFovLH(&matProjection,
+		(FLOAT)D3DXToRadian(45),
+		(FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT,
+		1.0f,
+		100.0f);
+
+	constantBuffer.finalTransformationMatrix = matRotate * matView * matProjection;
+	constantBuffer.rotationMatrix = matRotate;
+
+	d3dDeviceContext->RSSetState(rasterizerState);
+	d3dDeviceContext->PSSetSamplers(0, 1, &samplerState);
+	d3dDeviceContext->OMSetBlendState(blendState, 0, 0xffffffff);
+	d3dDeviceContext->ClearRenderTargetView(backBuffer, D3DXCOLOR(0.0f, 0.2f, 0.4f, 1.0f));
+	d3dDeviceContext->ClearDepthStencilView(depthBuffer, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	d3dDeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+	d3dDeviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	d3dDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	d3dDeviceContext->UpdateSubresource(constBuffer, NULL, NULL, &constantBuffer, 0, 0);
+	d3dDeviceContext->PSSetShaderResources(0, 1, &texture);
+	d3dDeviceContext->DrawIndexed(36, 0, 0);
+
+	swapÑhain->Present(0, NULL);
 }
 
 void CleanD3D(void)
 {
-	swapchain->SetFullscreenState(FALSE, NULL);
-	swapchain->Release();
-	backbuffer->Release();
-	dev->Release();
-	devcon->Release();
+	swapÑhain->SetFullscreenState(FALSE, NULL);
+
+	depthBuffer->Release();
+	inputLayout->Release();
+	vertexShader->Release();
+	pixelShader->Release();
+	vertexBuffer->Release();
+	indexBuffer->Release();
+	constBuffer->Release();
+	texture->Release();
+	swapÑhain->Release();
+	backBuffer->Release();
+	d3dDeviceInterface->Release();
+	d3dDeviceContext->Release();
+}
+
+void InitGraphics()
+{
+	VERTEX OurVertices[] =
+	{
+		{ -1.0f, -1.0f, 1.0f, D3DXVECTOR3(0.0f, 0.0f, 1.0f), 0.0f, 0.0f },
+		{ 1.0f, -1.0f, 1.0f, D3DXVECTOR3(0.0f, 0.0f, 1.0f), 0.0f, 1.0f },
+		{ -1.0f, 1.0f, 1.0f, D3DXVECTOR3(0.0f, 0.0f, 1.0f), 1.0f, 0.0f },
+		{ 1.0f, 1.0f, 1.0f, D3DXVECTOR3(0.0f, 0.0f, 1.0f), 1.0f, 1.0f },
+
+		{ -1.0f, -1.0f, -1.0f, D3DXVECTOR3(0.0f, 0.0f, -1.0f), 0.0f, 0.0f },
+		{ -1.0f, 1.0f, -1.0f, D3DXVECTOR3(0.0f, 0.0f, -1.0f), 0.0f, 1.0f },
+		{ 1.0f, -1.0f, -1.0f, D3DXVECTOR3(0.0f, 0.0f, -1.0f), 1.0f, 0.0f },
+		{ 1.0f, 1.0f, -1.0f, D3DXVECTOR3(0.0f, 0.0f, -1.0f), 1.0f, 1.0f },
+
+		{ -1.0f, 1.0f, -1.0f, D3DXVECTOR3(0.0f, 1.0f, 0.0f), 0.0f, 0.0f },
+		{ -1.0f, 1.0f, 1.0f, D3DXVECTOR3(0.0f, 1.0f, 0.0f), 0.0f, 1.0f },
+		{ 1.0f, 1.0f, -1.0f, D3DXVECTOR3(0.0f, 1.0f, 0.0f), 1.0f, 0.0f },
+		{ 1.0f, 1.0f, 1.0f, D3DXVECTOR3(0.0f, 1.0f, 0.0f), 1.0f, 1.0f },
+
+		{ -1.0f, -1.0f, -1.0f, D3DXVECTOR3(0.0f, -1.0f, 0.0f), 0.0f, 0.0f },
+		{ 1.0f, -1.0f, -1.0f, D3DXVECTOR3(0.0f, -1.0f, 0.0f), 0.0f, 1.0f },
+		{ -1.0f, -1.0f, 1.0f, D3DXVECTOR3(0.0f, -1.0f, 0.0f), 1.0f, 0.0f },
+		{ 1.0f, -1.0f, 1.0f, D3DXVECTOR3(0.0f, -1.0f, 0.0f), 1.0f, 1.0f },
+
+		{ 1.0f, -1.0f, -1.0f, D3DXVECTOR3(1.0f, 0.0f, 0.0f), 0.0f, 0.0f },
+		{ 1.0f, 1.0f, -1.0f, D3DXVECTOR3(1.0f, 0.0f, 0.0f), 0.0f, 1.0f },
+		{ 1.0f, -1.0f, 1.0f, D3DXVECTOR3(1.0f, 0.0f, 0.0f), 1.0f, 0.0f },
+		{ 1.0f, 1.0f, 1.0f, D3DXVECTOR3(1.0f, 0.0f, 0.0f), 1.0f, 1.0f },
+
+		{ -1.0f, -1.0f, -1.0f, D3DXVECTOR3(-1.0f, 0.0f, 0.0f), 0.0f, 0.0f },
+		{ -1.0f, -1.0f, 1.0f, D3DXVECTOR3(-1.0f, 0.0f, 0.0f), 0.0f, 1.0f },
+		{ -1.0f, 1.0f, -1.0f, D3DXVECTOR3(-1.0f, 0.0f, 0.0f), 1.0f, 0.0f },
+		{ -1.0f, 1.0f, 1.0f, D3DXVECTOR3(-1.0f, 0.0f, 0.0f), 1.0f, 1.0f },
+	};
+
+	D3D11_BUFFER_DESC bufferDescription;
+	ZeroMemory(&bufferDescription, sizeof(bufferDescription));
+
+	bufferDescription.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDescription.ByteWidth = sizeof(VERTEX) * 24;
+	bufferDescription.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDescription.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	d3dDeviceInterface->CreateBuffer(&bufferDescription, NULL, &vertexBuffer);
+	D3D11_MAPPED_SUBRESOURCE mappedSubresource;
+	d3dDeviceContext->Map(vertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &mappedSubresource);
+	memcpy(mappedSubresource.pData, OurVertices, sizeof(OurVertices));
+	d3dDeviceContext->Unmap(vertexBuffer, NULL);
+
+	DWORD OurIndices[] =
+	{
+		0, 1, 2,
+		2, 1, 3,
+		4, 5, 6,
+		6, 5, 7,
+		8, 9, 10,
+		10, 9, 11,
+		12, 13, 14,
+		14, 13, 15,
+		16, 17, 18,
+		18, 17, 19,
+		20, 21, 22,
+		22, 21, 23,
+	};
+
+	bufferDescription.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDescription.ByteWidth = sizeof(DWORD) * 36;
+	bufferDescription.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bufferDescription.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	bufferDescription.MiscFlags = 0;
+
+	d3dDeviceInterface->CreateBuffer(&bufferDescription, NULL, &indexBuffer);
+
+	d3dDeviceContext->Map(indexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &mappedSubresource);
+	memcpy(mappedSubresource.pData, OurIndices, sizeof(OurIndices));
+	d3dDeviceContext->Unmap(indexBuffer, NULL);
+
+	D3DX11CreateShaderResourceViewFromFile(d3dDeviceInterface,
+		L"E:\\bricks.png",
+		NULL,
+		NULL,
+		&texture,
+		NULL);
+}
+
+void InitPipeline()
+{
+	ID3D10Blob *VS, *PS;
+	D3DX11CompileFromFile(L"shaders.shader", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, 0, 0);
+	D3DX11CompileFromFile(L"shaders.shader", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, 0, 0);
+
+	d3dDeviceInterface->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &vertexShader);
+	d3dDeviceInterface->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pixelShader);
+	d3dDeviceContext->VSSetShader(vertexShader, 0, 0);
+	d3dDeviceContext->PSSetShader(pixelShader, 0, 0);
+
+	D3D11_INPUT_ELEMENT_DESC inputElementDescription[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	d3dDeviceInterface->CreateInputLayout(inputElementDescription, 3, VS->GetBufferPointer(), VS->GetBufferSize(), &inputLayout);
+	d3dDeviceContext->IASetInputLayout(inputLayout);
+
+	D3D11_BUFFER_DESC buffDesc;
+	ZeroMemory(&buffDesc, sizeof(buffDesc));
+	buffDesc.Usage = D3D11_USAGE_DEFAULT;
+	buffDesc.ByteWidth = 176;
+	buffDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	d3dDeviceInterface->CreateBuffer(&buffDesc, NULL, &constBuffer);
+	d3dDeviceContext->VSSetConstantBuffers(0, 1, &constBuffer);
+}
+
+void InitStates()
+{
+	D3D11_RASTERIZER_DESC rasterizerDescription;
+	rasterizerDescription.FillMode = D3D11_FILL_SOLID;
+	rasterizerDescription.CullMode = D3D11_CULL_BACK;
+	rasterizerDescription.FrontCounterClockwise = FALSE;
+	rasterizerDescription.DepthClipEnable = TRUE;
+	rasterizerDescription.ScissorEnable = FALSE;
+	rasterizerDescription.AntialiasedLineEnable = FALSE;
+	rasterizerDescription.MultisampleEnable = FALSE;
+	rasterizerDescription.DepthBias = 0;
+	rasterizerDescription.DepthBiasClamp = 0.0f;
+	rasterizerDescription.SlopeScaledDepthBias = 0.0f;
+	d3dDeviceInterface->CreateRasterizerState(&rasterizerDescription, &rasterizerState);
+
+	D3D11_SAMPLER_DESC samplerDescription;
+	samplerDescription.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDescription.MaxAnisotropy = 16;
+	samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.BorderColor[0] = 0.0f;
+	samplerDescription.BorderColor[1] = 0.0f;
+	samplerDescription.BorderColor[2] = 0.0f;
+	samplerDescription.BorderColor[3] = 0.0f;
+	samplerDescription.MinLOD = 0.0f;
+	samplerDescription.MaxLOD = FLT_MAX;
+	samplerDescription.MipLODBias = 0.0f;
+	d3dDeviceInterface->CreateSamplerState(&samplerDescription, &samplerState);
+
+	D3D11_BLEND_DESC blendDescription;
+	blendDescription.RenderTarget[0].BlendEnable = TRUE;
+	blendDescription.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+	blendDescription.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
+	blendDescription.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
+	blendDescription.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	blendDescription.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+	blendDescription.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+	blendDescription.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	blendDescription.IndependentBlendEnable = FALSE;
+	blendDescription.AlphaToCoverageEnable = FALSE;
+
+	d3dDeviceInterface->CreateBlendState(&blendDescription, &blendState);
 }
---
+++ TestingApp/TestingApp/shaders.shader
@@ -0,0 +1,36 @@
+cbuffer ConstantBuffer
+{
+    float4x4 finalTransformationMatrix;
+    float4x4 rotationMatrix;
+    float4 directionalLightVector;
+    float4 directionalLightColor;
+    float4 ambientLightColor;
+}
+
+Texture2D Texture;
+SamplerState samplerState;
+
+struct VOut
+{
+    float4 color : COLOR;
+    float2 textureCoordinates : TEXCOORD;
+    float4 position : SV_POSITION;
+};
+
+VOut VShader(float4 position : POSITION, float4 normal : NORMAL, float2 textureCoordinates : TEXCOORD)
+{
+    VOut output;
+    output.position = mul(finalTransformationMatrix, position);
+    output.color = ambientLightColor;
+    float4 rotatedNormal = normalize(mul(rotationMatrix, normal));
+    float directionalBrightness = saturate(dot(rotatedNormal, directionalLightVector));
+    output.color += directionalLightColor * directionalBrightness;
+    output.textureCoordinates = textureCoordinates;
+    return output;
+}
+
+float4 PShader(float4 color : COLOR, float2 textureCoordinates : TEXCOORD) : SV_TARGET
+{
+    float4 texturedColor = color * Texture.Sample(samplerState, textureCoordinates);
+    return texturedColor;
+}
